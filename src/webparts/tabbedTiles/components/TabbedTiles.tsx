64b8e169e2f2ddd86258d077261c2f9da0b72dc2
Thomas Berman <thomas.berman@cadenzasoftware.com>
2019-04-17 15:18:58 +0100

    messed up the code big time

--- src/webparts/tabbedTiles/components/TabbedTiles.tsx
+++ src/webparts/tabbedTiles/components/TabbedTiles.tsx
@@ -7,19 +7,7 @@ export default class TabbedTiles extends React.Component<ITabbedTilesProps, {}> 
   public render(): React.ReactElement<ITabbedTilesProps> {
     return (
       <div className={ styles.tabbedTiles }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
+        <
     );
   }
 }
